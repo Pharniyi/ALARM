99560c0bea8a8ae9dd910a6345bb70dc80941865
Pharniyi <99425154+Pharniyi@users.noreply.github.com>
2024-04-15 14:47:55 -0700

    Add files via upload

--- js/alarm.js
+++ js/alarm.js
@@ -1,4 +1,10 @@
-const selectMenu = document.querySelectorAll('select')
+const currentTime = document.querySelector('h1')
+content= document.querySelector('.content')
+selectMenu = document.querySelectorAll('select')
+setAlarmbutton= document.querySelector('button')
+
+let alarmTime,isAlarmSet=false,
+ringtone =new Audio("./assets/alarm.mp3");
 
 for(let i=12; i>0 ;i--){
     i= i<10 ? "0" + i : i;
@@ -13,15 +19,62 @@ for(let i=59; i>=0 ;i--){
 }
 
 for(let i=2; i>0 ;i--){
-   /*let ampm=i;
-   if (ampm=1){
+  /* let ampm;
+   if (i==1){
     let ampm="AM"
     
    }
    else{
     let ampm="PM"
    }*/
-   let ampm= i == 1 ? "AM" : "PM" ;
+    let ampm= i == 1 ? "AM" : "PM" ;
     let option = `<option value="${ampm}">${ampm}</option>`;
     selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
 }
+
+setInterval(() => {
+    let date=new Date(),
+    h= date.getHours(),
+    m= date.getMinutes(),
+    s= date.getSeconds(),
+    ampm= "AM";
+
+    if(h >= 12){
+        h=h - 12;
+        ampm= "PM";
+    }
+
+    h= h == 0 ? h= 12 : h
+
+    h= h < 10 ? "0" + h : h
+    m= m < 10 ? "0" + m : m
+    s= s < 10 ? "0" + s : s
+
+    currentTime.innerText = `${h} : ${m} : ${s} ${ampm}`;
+    /*console.log(`${h} : ${m} : ${s} ${ampm}`)*/
+
+    if (alarmTime == `${h} : ${m} : ${s} ${ampm}`){
+        ringtone.play();
+        ringtone.loop=true;
+    }
+},1000);
+
+function setAlarm(){
+    if(isAlarmSet){
+        alarmTime="";
+        ringtone.pause();
+        content.classList.remove("disabled");
+        setAlarmbutton.innerText= "Set Alarm"
+        return isAlarmSet= false;
+    }
+    let time= `${selectMenu[0].value} : ${selectMenu[1].value}  ${selectMenu[2].value}`
+    if(time.includes("Hour") || time.includes("Minutes") || time.includes("AM/PM")){
+        return alert("Please,select a valid time to set Alarm!");
+    }
+    isAlarmSet=true 
+    alarmTime= time
+    content.classList.add("disabled");
+    setAlarmbutton.innerText= "clear Alarm"
+    console.log(time)
+}
+setAlarmbutton.addEventListener('click' , setAlarm); 
